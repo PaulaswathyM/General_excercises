d5433a43a9372d696e8e98d86d291881e2653022
PaulaswathyM <aswathy.paul91@gmail.com>
2023-11-20 10:50:55 +0530

    The transcripts ids are mapped to gene symbols

---
+++ Mapping_gene_symbols_ensembl.R
@@ -0,0 +1,18 @@
+#how to map a list of ensembl transcript ids to gene symbol
+#first read the file with ids into an object
+
+Ids<- read.csv("D:/Capulus_thera/BLOG_WRITEUPS/AR_V7_correlated_ids_0_3_greater.txt")
+
+#to map the ids we will be using biomart library. Load the library
+
+library(biomaRt)
+mart <- useMart(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
+
+res <- getBM(attributes = c('ensembl_transcript_id_version', 
+                            'ensembl_gene_id', 
+                            'external_transcript_name',
+                            'external_gene_name'),
+             filters = 'ensembl_transcript_id_version', 
+             values = Ids,
+             mart = mart)
+head(res)
